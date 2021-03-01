cf1b6cc16b5ae62eee00dd6beaabbba7d32cd96c
Bilal <b.umrani75@gmail.com>
2021-03-01 07:23:31 +0000

    streams

--- 12-streams/index.js
+++ 12-streams/index.js
@@ -4,4 +4,14 @@
 
 // Readable: Stream which is used for read operation
 // Writeable: Stream which is used for write operation
-// 
+// Duplex: stream which can be used for both read and write operation
+// Transform: A type of dublex stream where the output is computed based on the input
+
+// Each type of stream is an EventEmitter instance and throws several
+// events on different instance of time for example some of the commonly
+// used events are:
+
+// data: This event is fired when there is data availbale to read.
+// end: This event is fired when there is no more data available to read.
+// error: This event is fired when there is an error in sending or recieving data.
+// finish: Thid event is fired when all the data haa been flushed
